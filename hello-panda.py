01837e0994baf0808489a5800ccff4f41f86e83c
Nathan Krasney <natankrasney@gmail.com>
2021-01-12 20:58:43 +0200

    ...

--- hello-panda.py
+++ hello-panda.py
@@ -5,6 +5,6 @@ d = {'col1': [1, 2], 'col2': [3, 4]}
 df1 = pd.DataFrame(d)
 print("df1 : \n",df1)
 
-df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-                   columns=['aaa', 'bbb', 'ccc'])
+df2 = pd.DataFrame(np.array([[1, 2, 3, 4], [4, 5, 6 , 7], [7, 8, 9 , 10]]),
+                   columns=['aaa', 'bbb', 'ccc' , 'ddd'])
 print("df2 : \n",df2)
