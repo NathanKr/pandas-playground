5dbac21336734a8f882c547fdc848094f69d9305
Nathan Krasney <natankrasney@gmail.com>
2021-03-07 18:14:26 +0200

    ...

--- iterate.py
+++ iterate.py
@@ -4,5 +4,6 @@ df = pd.DataFrame([[1, 2, 3, 4], [4, 5, 6 , 7], [7, 8, 9 , 10]],
                    columns=['col1', 'col2', 'col3' , 'col4'])
 print("df : \n",df)
 
+print("\niterate col1")
 for i in df.index: 
     print(df['col1'][i]) 
