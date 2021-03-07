4f1da6a4f55fdfb41e4fbc2713d964395d9db1ad
Nathan Krasney <natankrasney@gmail.com>
2021-03-07 16:55:43 +0200

    ...

--- series.py
+++ series.py
@@ -7,7 +7,7 @@ print(f"s1\n{s1}")
 s2 = pd.Series(['A' , 'B' , 'C'], index = labels)
 print(f"s2\n{s2}")
 
-df = pd.DataFrame(data=[],columns=labels)
+df = pd.DataFrame([],columns=labels)
 df = df.append(s1,ignore_index=True)
 df = df.append(s2,ignore_index=True)
 
