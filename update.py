100293f17968eed4177e81465db8dd57975faa7a
Nathan Krasney <natankrasney@gmail.com>
2021-03-08 12:21:20 +0200

    update

---
+++ update.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.DataFrame([[1, 2, 3, 4], [4, 5, 6 , 7], [7, 8, 9 , 10]],
+                   columns=['col1', 'col2', 'col3' , 'col4'])
+print("df before update : \n",df)
+
+# use iloc to update first col in second row
+df.iloc[1,0]=99
+print("df after first update : \n",df)
+
+# use loc to update second col in third row
+df.loc[2,'col2']=99
+print("df after second update : \n",df)
+
