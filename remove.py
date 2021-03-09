8064737afa1aebfb9b592181be59df38aa26cb03
Nathan Krasney <natankrasney@gmail.com>
2021-03-09 18:14:58 +0200

    remove

---
+++ remove.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.DataFrame([[1, 2, 3, 4], [4, 5, 6 , 7], [7, 8, 9 , 10]],
+                   columns=['col1', 'col2', 'col3' , 'col4'])
+print("df : \n",df)
+
+
+# remove first row
+df.drop([0],inplace=True)
+print("df : \n",df)
