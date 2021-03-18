af72aa4449ca6c359a55b6d52e92369c6edf7a2a
Nathan Krasney <natankrasney@gmail.com>
2021-03-18 14:31:25 +0200

    read_html

--- read_html_try.py
+++ read_html_try.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-snp500=pd.read_html(url)[0]
-symbols =  snp500.Symbol 
+snp500_df = pd.read_html(url)[0]
+symbols =  snp500_df . Symbol 
 print(symbols)
