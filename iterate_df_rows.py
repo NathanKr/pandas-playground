868a1fb5ca5c855dc1a03acf19fd6426f64facf1
Nathan Krasney <natankrasney@gmail.com>
2021-06-22 13:07:21 +0300

    better iterate rows

--- iterate_df_rows.py
+++ iterate_df_rows.py
@@ -5,8 +5,10 @@ df = pd.DataFrame([[1, 2, 3, 4], [4, 5, 6 , 7], [7, 8, 9 , 10]],
 print("df : \n",df)
 
 print('\niterate on rows')
-for i, row in df.iterrows():
-    print(f'i : {i} , row[0] : {row[0]}')
+for i_row, row in df.iterrows():
+    print(f'row : {i_row}')
+    for i_col in range(len(row)):
+        print(f'row[{i_col}] : {row[i_col]}')
 
 
 
