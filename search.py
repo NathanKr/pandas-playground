90a57c747598592a8ea2b2ace10aaeb5c80d07af
Nathan Krasney <natankrasney@gmail.com>
2021-06-30 18:02:31 +0300

    search and processing

---
+++ search.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+dict = {'col1' : [10,20,33] , 
+        'col2' : [11,21,31]
+       }
+
+df = pd.DataFrame(dict)
+
+print(f'df\n{df}')
+
+# filter dataframe
+filt_df = df > 19
+print (f'\nfilt_df\n{filt_df}') # return same size df with boolean
+print(f'\ndf[filt_df]\n{df[filt_df]}')
+
+# filter series
+series_col_1 = df['col1']
+filt_series = series_col_1 > 19
+print(f'\nfilt_series\n{filt_series}')
+print(f'\nseries_col_1[filt_series]\n{series_col_1[filt_series]}')
+
+
---
+++ series_processing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# get the item which is integer
+s1 = pd.Series(['$' , '11' , 'a1',''])
+print(f'\ns1\n{s1}')
+res_bool : pd.Series = s1.apply(lambda string : string.isnumeric())
+print(f'\nis numeric in series : {res_bool}')
+
+# get the numeric based on res
+res_series = s1[res_bool]
+print(f'\nres_series : {res_series}')
+print(f'\nres_series.array : {res_series.array}')
+print(f'\ntype(res_series) : {type(res_series)}')
