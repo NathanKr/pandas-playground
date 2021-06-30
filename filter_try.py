3ef6a36002b8a0e9d12aa7b823dac976a93ac8b3
Nathan Krasney <natankrasney@gmail.com>
2021-06-30 21:34:48 +0300

    search

---
+++ filter_try.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+dict = {'col1' : [10,17,20] , 
+        'col2' : [11,21,31]}
+
+df = pd.DataFrame(dict)
+print(f'\ndf\n{df}')
+
+
+# search for values > 19 in the dataframe
+print (f'\ndf[df > 19]\n{df[df > 19]}')
+
+
+# search for values using logical and
+filt = (df > 19) & (df < 22)
+print (f'\ndf[filt]\n{df[filt]}')
+print (f'\ndf[~filt]\n{df[~filt]}') # not ( > 19 and < 22)
+
+
+
