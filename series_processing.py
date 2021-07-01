fedac816f3878e0624b9204675f2eaa62119fa90
Nathan Krasney Laptop <natankrasney@gmail.com>
2021-07-01 05:59:27 +0300

    ...

--- series_processing.py
+++ series_processing.py
@@ -3,11 +3,11 @@ import pandas as pd
 # get the string which represent integers
 s1 = pd.Series(['$' , '11' , 'a1', '123'])
 print(f'\ns1\n{s1}')
-res_bool : pd.Series = s1.apply(lambda string : string.isnumeric())
-print(f'\nis numeric in series : {res_bool}')
+filt : pd.Series = s1.apply(lambda string : string.isnumeric())
+print(f'\nis numeric in series using filt: {filt}')
 
 # get the numeric based on res
-res_series = s1[res_bool]
+res_series = s1[filt]
 print(f'\nres_series : {res_series}')
 print(f'\nres_series.array : {res_series.array}')
 print(f'\ntype(res_series) : {type(res_series)}')
