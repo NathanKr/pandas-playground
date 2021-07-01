6ff0774f7422023af3de9311543d681d4c5f6d05
Nathan Krasney Laptop <natankrasney@gmail.com>
2021-07-01 05:46:13 +0300

    ...

--- slice_series.py
+++ slice_series.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
 series = pd.Series([1,2,35,4,5])
-print(f'series : {series}')
-print(f'series.to_numpy() : {series.to_numpy()}')
+print(f'\nseries\n{series}')
+print(f'\nseries.to_numpy()\n{series.to_numpy()}')
 
 filt = [True,False,True,False,False]
 print(f'\nseries[filt]\n{series[filt]}')
+
+print(f'\nseries[1:3]\n{series[1:3]}')
