75874771e6195bdbbac5164c1c4f286caad1777f
Nathan Krasney Laptop <natankrasney@gmail.com>
2021-07-01 07:16:41 +0300

    remove nan

--- df_to_series.py
+++ df_to_series.py
@@ -5,15 +5,9 @@ dict={'col1' : [1,np.nan,-3] , 'col2':[3,-6,np.nan]}
 df = pd.DataFrame(dict)
 print(f'\ndf\n{df}')
 
-# apply len for each row
-print(f'apply len per\n{df.apply(len,axis=1)}')
 
-# get non nan on row
-# print(df.loc[1].dropna())
+print(f'\nfirst not nan on row\n{df.apply(lambda row_series : row_series.dropna()[0] ,axis=1)}')
 
-# get first not nan on row
+print(f'\nfirst not nan on col\n{df.apply(lambda row_series : row_series.dropna()[0] ,axis=0)}')
 
-# get non nan on col
-
-# get first not nan on col
 
